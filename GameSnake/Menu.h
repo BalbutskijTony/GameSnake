37a3a482d6919db37fa572938a31e9df7e2a7a00
lalovkos <lalovkos@gmail.com>
2020-01-12 00:52:39 +0700

    Initial commit

---
+++ GameSnake/Menu.h
@@ -0,0 +1,50 @@
+#pragma once
+#pragma once
+
+using namespace sf;
+
+void MenuFunc(RenderWindow& window) {
+	Texture NewGame, MultiGame, Exit, Background, Background2;
+	NewGame.loadFromFile("images/NewGame.png");
+	MultiGame.loadFromFile("images/Multi.png");
+	Exit.loadFromFile("images/Exit.png");
+	Background.loadFromFile("images/Background.jpg");
+	Background2.loadFromFile("images/Background2.png");
+	RenderWindow
+		Sprite menu1(NewGame), menu2(MultiGame), menu3(Exit), about(Background), menuBg(Background2);
+	bool isMenu = 1;
+	int menuNum = 0;
+	menu1.setPosition(100, 30);
+	menu2.setPosition(100, 90);
+	menu3.setPosition(100, 150);
+	menuBg.setPosition(345, 0);
+
+	//////////////////////////////МЕНЮ///////////////////
+	while (isMenu)
+	{
+		menu1.setColor(Color::White);
+		menu2.setColor(Color::White);
+		menu3.setColor(Color::White);
+		menuNum = 0;
+		window.clear(Color(129, 181, 221));
+
+		if (IntRect(100, 30, 300, 50).contains(Mouse::getPosition(window))) { menu1.setColor(Color::Blue); menuNum = 1; }
+		if (IntRect(100, 90, 300, 50).contains(Mouse::getPosition(window))) { menu2.setColor(Color::Blue); menuNum = 2; }
+		if (IntRect(100, 150, 300, 50).contains(Mouse::getPosition(window))) { menu3.setColor(Color::Blue); menuNum = 3; }
+
+		if (Mouse::isButtonPressed(Mouse::Left))
+		{
+			if (menuNum == 1) isMenu = false;//если нажали первую кнопку, то выходим из меню 
+			if (menuNum == 2) { window.draw(about); window.display(); while (!Keyboard::isKeyPressed(Keyboard::Escape)); }
+			if (menuNum == 3) { window.close(); isMenu = false; }
+
+		}
+
+		window.draw(menuBg);
+		window.draw(menu1);
+		window.draw(menu2);
+		window.draw(menu3);
+
+		window.display();
+	}
+}
