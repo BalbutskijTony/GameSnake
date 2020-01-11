a19abe1b768bc235428003333cad6347f14d11fa
BalbutskijTony <lesnik_and@mail.ru>
2020-01-11 22:04:29 +0700

    Балбуцкий. Добавлена возможность ограничения/растяжения игрового цикла одной секундой.

--- GameSnake/main.cpp
+++ GameSnake/main.cpp
@@ -1,13 +1,14 @@
 #include <vector>
-#include <list>
 #include <chrono>
+#include <future> 
 
 #include "Map.h"
 #include "Painter.h"
 
 void gameTic(Player& player) {
-
+    player.move();
 }
+
 
 int main()
 {
@@ -15,20 +16,33 @@ int main()
     Map map(64, 32);
     Painter painter;
     Player player;
+    sf::Event event;
+
+    std::chrono::time_point<std::chrono::steady_clock> startGameTic, endGameTic;
+    
+    
+
 
     while (window.isOpen())
     {
-        sf::Event event;
+        startGameTic = std::chrono::steady_clock::now();
+       
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
+        // TODO: Переделать
+        // TODO: Разобраться в чём отличие wait_for от wait_until
+        std::future<void> nextTic = std::async([&player] { gameTic(player); });
+        nextTic.wait_for(std::chrono::seconds(1));
         window.clear();
         painter.drawGrid(map, window);
         painter.drawPlayer(player, map, window);
         window.display();
+
+        endGameTic = std::chrono::steady_clock::now();
+        std::this_thread::sleep_until(startGameTic + std::chrono::seconds(1));
     }
     return 0;
 }
