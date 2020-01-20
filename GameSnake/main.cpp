80ab63f38c0130b38e69ce95f449999c2ce0591b
BalbutskijTony <lesnik_and@mail.ru>
2020-01-20 21:06:43 +0700

    Балбуцкий. Добавлена возможность многопользовательской(2 игрока) игры на одном компьютере.

--- GameSnake/main.cpp
+++ GameSnake/main.cpp
@@ -5,7 +5,61 @@
 #include "Game.h"
 #include "Painter.h"
 
+enum class MoveAction
+{
+    LEFT,
+    RIGTH,
+    DOWN,
+    UP,
+    NO_ACTION
+};
 
+enum class AbilityAction
+{
+    USE_1_ABILITY,
+    USE_2_ABILITY,
+    USE_3_ABILITY,
+    USE_4_ABILITY,
+    USE_5_ABILITY,
+    USE_6_ABILITY
+};
+
+class PlayerController {
+public:
+    std::pair<MoveAction, sf::Keyboard::Key> left;
+    std::pair<MoveAction, sf::Keyboard::Key> right;
+    std::pair<MoveAction, sf::Keyboard::Key> down;
+    std::pair<MoveAction, sf::Keyboard::Key> up;
+};
+
+void buttonClickHandler(const std::vector<PlayerController>& controllers, const sf::Keyboard::Key button, Game& game) {
+    for (int curPlayerIndex = 0; curPlayerIndex < controllers.size(); curPlayerIndex++) {
+        if (button == controllers[curPlayerIndex].left.second && !game.getPlayer(curPlayerIndex).getIsPlayerMove()) {
+            if (game.setNewDirection(curPlayerIndex, Point2d(-1, 0))) {
+                game.setIsPlayerMove(curPlayerIndex, true);
+                return;
+            }
+        }
+        if (button == controllers[curPlayerIndex].right.second && !game.getPlayer(curPlayerIndex).getIsPlayerMove()) {
+            if (game.setNewDirection(curPlayerIndex, Point2d(1, 0))) {
+                game.setIsPlayerMove(curPlayerIndex, true);
+                return;
+            }
+        }
+        if (button == controllers[curPlayerIndex].up.second && !game.getPlayer(curPlayerIndex).getIsPlayerMove()) {
+            if (game.setNewDirection(curPlayerIndex, Point2d(0, -1))) {
+                game.setIsPlayerMove(curPlayerIndex, true);
+                return;
+            }
+        }
+        if (button == controllers[curPlayerIndex].down.second && !game.getPlayer(curPlayerIndex).getIsPlayerMove()) {
+            if (game.setNewDirection(curPlayerIndex, Point2d(0, 1))) {
+                game.setIsPlayerMove(curPlayerIndex, true);
+                return;
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -16,8 +70,22 @@ int main()
     Painter painter;
     
     Game newGame(map);
-    int curPlayerIndex = newGame.addNewPlayer(Player(Point2d(5, 5), Point2d(1, 0)));
+    int player1Index = newGame.addNewPlayer(Player(Point2d(5, 5), Point2d(1, 0)));
+    int player2Index = newGame.addNewPlayer(Player(Point2d(55, 25), Point2d(-1, 0)));
     
+    std::vector<PlayerController> controllers = { PlayerController(), PlayerController() };
+
+    controllers[0].down = std::make_pair(MoveAction::DOWN, sf::Keyboard::S);
+    controllers[0].left = std::make_pair(MoveAction::LEFT, sf::Keyboard::A);
+    controllers[0].right = std::make_pair(MoveAction::RIGTH, sf::Keyboard::D);
+    controllers[0].up = std::make_pair(MoveAction::UP, sf::Keyboard::W);
+
+    controllers[1].down = std::make_pair(MoveAction::DOWN, sf::Keyboard::Down);
+    controllers[1].left = std::make_pair(MoveAction::LEFT, sf::Keyboard::Left);
+    controllers[1].right = std::make_pair(MoveAction::RIGTH, sf::Keyboard::Right);
+    controllers[1].up = std::make_pair(MoveAction::UP, sf::Keyboard::Up);
+
+
 
     sf::Event event;
     bool isGame = true;
@@ -29,7 +97,8 @@ int main()
     while (window.isOpen())
     {
         startGameTic = std::chrono::steady_clock::now();
-        isPlayerMove = false;
+        for (int curPlayerIndex = 0; curPlayerIndex < controllers.size(); curPlayerIndex++)
+            newGame.setIsPlayerMove(curPlayerIndex, false);
         while (window.pollEvent(event))
         {
             
@@ -41,22 +110,7 @@ int main()
                 // Получаем нажатую клавишу - выполняем соответствующее действие
                 if (event.key.code == sf::Keyboard::Escape) window.close();
                 if (isGame) {
-                    if (event.key.code == sf::Keyboard::Left && !isPlayerMove) {
-                        if(newGame.setNewDirection(curPlayerIndex, Point2d(-1, 0)))
-                            isPlayerMove = true;
-                    }
-                    if (event.key.code == sf::Keyboard::Right && !isPlayerMove) {
-                        if (newGame.setNewDirection(curPlayerIndex, Point2d(1, 0)))
-                            isPlayerMove = true;
-                    }
-                    if (event.key.code == sf::Keyboard::Up && !isPlayerMove) {
-                        if (newGame.setNewDirection(curPlayerIndex, Point2d(0, -1)))
-                            isPlayerMove = true;
-                    }
-                    if (event.key.code == sf::Keyboard::Down && !isPlayerMove) {
-                        if (newGame.setNewDirection(curPlayerIndex, Point2d(0, 1)))
-                            isPlayerMove = true;
-                    }
+                    buttonClickHandler(controllers, event.key.code, newGame);
                 }
             }
         }
@@ -67,12 +121,20 @@ int main()
             nextTic.wait_for(std::chrono::milliseconds(200));
             window.clear();
             painter.drawGrid(map, window);
-            painter.drawPlayer(newGame.getPlayer(curPlayerIndex), newGame.getMap(), window);
+            for(int curPlayerIndex = 0; curPlayerIndex < controllers.size(); curPlayerIndex++)
+                if(newGame.getPlayer(curPlayerIndex).getAlive())
+                    painter.drawPlayer(newGame.getPlayer(curPlayerIndex), newGame.getMap(), window);
             painter.drawApples(newGame.getApples(), newGame.getMap(), window);
             painter.drawWalls(newGame.getMap(), window);
             window.display();
 
-            if (newGame.isPlayerCollide(curPlayerIndex)) isGame = false;
+            isGame = false;
+            for (int curPlayerIndex = 0; curPlayerIndex < controllers.size(); curPlayerIndex++) {
+                if (newGame.isPlayerCollide(curPlayerIndex)) {
+                    newGame.setAlive(curPlayerIndex, false);
+                }
+                isGame = isGame || newGame.getPlayer(curPlayerIndex).getAlive();
+            }
             endGameTic = std::chrono::steady_clock::now();
             std::this_thread::sleep_until(startGameTic + std::chrono::milliseconds(200));
         }
